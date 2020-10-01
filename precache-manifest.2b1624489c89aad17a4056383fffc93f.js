6adf28d02c2c79618a7a57ec055f2bd3db488526
mukul-kmr-jha <jimickeyji@gmail.com>
2020-10-01 14:19:41 +0530

    Updates

---
+++ precache-manifest.2b1624489c89aad17a4056383fffc93f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddec5d6e8a02d8d411e12a7cba120d58",
+    "url": "/lf/index.html"
+  },
+  {
+    "revision": "e0b76f3e879ab05de1e4",
+    "url": "/lf/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7912d6ef2312677439ab",
+    "url": "/lf/static/css/main.f4871134.chunk.css"
+  },
+  {
+    "revision": "e0b76f3e879ab05de1e4",
+    "url": "/lf/static/js/2.2ea7c817.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lf/static/js/2.2ea7c817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7912d6ef2312677439ab",
+    "url": "/lf/static/js/main.8464a77b.chunk.js"
+  },
+  {
+    "revision": "6c5c94b2dcc805291367",
+    "url": "/lf/static/js/runtime-main.6c55eb13.js"
+  },
+  {
+    "revision": "79998795e750370ed6fb8f89fa4d55a2",
+    "url": "/lf/static/media/ProductImg.79998795.png"
+  }
+]);
